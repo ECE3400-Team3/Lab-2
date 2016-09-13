1cdb7cad07d4e2160e269e5d98c0ec24cebed706
abc248 <abc248@users.noreply.github.com>
2016-09-13 02:20:20 -0400

    Update fft_adc_serial.ino

--- fft_adc_serial.ino
+++ fft_adc_serial.ino
@@ -15,25 +15,28 @@ port at 115.2kb.
 void setup() {
   Serial.begin(115200); // use the serial port
   TIMSK0 = 0; // turn off timer0 for lower jitter
-  //ADCSRA = 0xe5; // set the adc to free running mode
-  //ADMUX = 0x40; // use adc0
-  //DIDR0 = 0x01; // turn off the digital input for adc0
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   while(1) { // reduces jitter
     cli();  // UDRE interrupt slows this way down on arduino1.0
-    int k;
     for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
-      //while(!(ADCSRA & 0x10)); // wait for adc to be ready
-      //ADCSRA = 0xf5; // restart adc
-      //byte m = ADCL; // fetch adc data
-      //byte j = ADCH;
-      k = analogRead(A0);
-      Serial.println(k);
-      //int k = (j << 8) | m; // form into an int
-      //k -= 0x0200; // form into a signed int
-      //k <<= 6; // form into a 16b signed int
+      while(!(ADCSRA & 0x10)) {
+        digitalWrite(12, HIGH); // wait for adc to be ready
+      }
+      digitalWrite(12, LOW); // sampling the ADC
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      //int k = analogRead(A0);
+      //Serial.println(k);
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
       fft_input[i] = k; // put real data into even bins
       fft_input[i+1] = 0; // set odd bins to 0
     }
@@ -41,11 +44,10 @@ void loop() {
     fft_reorder(); // reorder the data before doing the fft
     fft_run(); // process the data in the fft
     fft_mag_log(); // take the output of the fft
-    sei();
     Serial.println("start");
     for (byte i = 0 ; i < FFT_N/2 ; i++) { 
-      //Serial.println(fft_log_out[i]); // send out the data
-      delay(10000);
+      Serial.println(fft_log_out[i]); // send out the data
+      //delay(10000);
     }
   }
 }
